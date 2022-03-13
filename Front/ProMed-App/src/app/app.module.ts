d424f5f614f4bb9575f32998593eaeb25f3f18f7
lucaslopes2000 <lucassl2011@gmail.com>
2022-03-13 00:38:45 -0300

    Angular: Interpolação, Diretivas e Binding

--- Front/ProMed-App/src/app/app.module.ts
+++ Front/ProMed-App/src/app/app.module.ts
@@ -1,24 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HospitaisComponent } from './hospitais/hospitais.component';
 import { MedicosComponent } from './medicos/medicos.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component';
+
+import { CollapseModule } from 'ngx-bootstrap/collapse';
 
 @NgModule({
   declarations: [
     AppComponent,
     HospitaisComponent,
-    MedicosComponent
-  ],
+    MedicosComponent,
+    NavComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CollapseModule.forRoot(),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Front/ProMed-App/src/app/hospitais/hospitais.component.ts
+++ Front/ProMed-App/src/app/hospitais/hospitais.component.ts
@@ -8,7 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HospitaisComponent implements OnInit {
 
-  public hospitais: any;
+  public hospitais: any = [];
+  public hospitaisFiltrados: any = [];
+
+  widthImg: number = 100;
+  marginImg: number = 2;
+  exibirImg: boolean = true;
+  private _filtroLista: string = '';
+
+  public get filtroLista(): string {
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value: string) {
+    this._filtroLista = value;
+    this.hospitaisFiltrados = this.filtroLista ? this.filtrarHospitais(this.filtroLista) : this.hospitais;
+  }
+
+  filtrarHospitais(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.hospitais.filter(
+      (hospital: { nome: string; endereco: string; }) => hospital.nome.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+      hospital.endereco.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    )
+  }
 
   constructor(private http: HttpClient) { }
 
@@ -16,10 +39,17 @@ export class HospitaisComponent implements OnInit {
     this.getHospitais();
   }
 
+  estadoImg() {
+    this.exibirImg = !this.exibirImg;
+  }
+
   public getHospitais(): void {
     this.http.get('https://localhost:5001/api/hospitais').subscribe(
-      response => this.hospitais = response,
-      error => console.log(error)
+    response => {
+      this.hospitais = response;
+      this.hospitaisFiltrados = this.hospitais;
+    },
+    error => console.log(error)
     );
   }
 
